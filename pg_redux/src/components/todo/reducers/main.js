8839399df81b2f4f4c63ba2107116ce03cbf00c8
lequan <lequan.vn89@gmail.com>
2016-11-15 16:51:14 +0200

    Using Redux's `combineReducers`.

--- pg_redux/src/components/todo/reducers/main.js
+++ pg_redux/src/components/todo/reducers/main.js
@@ -1,10 +1,18 @@
+import { combineReducers } from 'redux';
+
 import todos from './todos';
 import visibilityFilter from './visibility-filter';
 
 
-export default (state = {}, action) => {
-    return {
-        todos: todos(state.todos, action),
-        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-    }
-}
+/* Without Redux's combineReducers */
+// export default (state = {}, action) => {
+//     return {
+//         todos: todos(state.todos, action),
+//         visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     }
+// }
+
+export default combineReducers({
+    todos,
+    visibilityFilter,
+})
